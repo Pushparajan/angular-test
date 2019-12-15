ca44249c439191a31e3e05eada918deb533f6e5f
Pushparajan <pushparajanr@gmail.com>
2019-12-15 08:55:10 -0600

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Pushparajan';
+  
+  todoList = [{id:1, name: 'task1', complete:true}, {id:2, name: 'task2', complete:false}];
+  
+  newTodo:any;
+
+  onAdd(todoName){
+    var newTodo = {id:this.todoList.length+1, name:todoName, complete:false};
+    this.todoList.push(newTodo);
+  }
+
+  onDelete(todo){
+    this.todoList = this.todoList.filter(t=> t.id != todo.id)
+  }
+
+
+
+
+
 }
